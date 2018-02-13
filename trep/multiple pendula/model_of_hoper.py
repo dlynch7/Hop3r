8187942b72990c5180efb5fc820ac9fe8037034f
Greg Niederschulte <gregoryniederschulte2018@u.northwestern.edu>
2018-02-13 11:46:43 -0600

    Velocities implemented successfully at actuated joints in "Model". "Model2" simply has torques at the actuated joints but has a jumping profile that looks right

--- trep/multiple pendula/model_of_hoper.py
+++ trep/multiple pendula/model_of_hoper.py
@@ -7,33 +7,12 @@ import math
 import numpy as np
 from math import sin
 
-
 dt = 0.01
 tf = 10.0
-
-def simulate_system(system, u_func):
-    # Now we'll extract the current configuration into a tuple to use as
-    # initial conditions for a variational integrator.
-    q0 = system.q
-
-    # Create and initialize the variational integrator
-    mvi = trep.MidpointVI(system)
-    mvi.initialize_from_configs(0.0, q0, dt, q0)
-    system.satisfy_constraints(tolerance=1e-1)
-
-    # This is our simulation loop.  We save the results in two lists.
-    q = [mvi.q2]
-    t = [mvi.t2]
-    while mvi.t1 < tf:
-        mvi.step(mvi.t2+dt, u_func(mvi.t1))
-        q.append(mvi.q2)
-        t.append(mvi.t2)
-    return (t,q)
-
 # Set up the system frames
 system = trep.System()
 system.import_frames([
-    rx('theta1'),[
+    rx('theta1', kinematic=True),[
         tz(-0.7, name='pend1'),[
         # tz(-0.35, name='pend1'),[
             rx('theta2'),[
@@ -43,13 +22,13 @@ system.import_frames([
                             tz(-0.5, mass=1, name='COMLeg'),[
                                 tz(-1, name='pend3')]]]]]]],
     ty(1), [
-        rx('theta4'),[
+        rx('theta4', kinematic=True),[
             tz(-0.5, mass=1, name='COM4'),[
                 tz(-0.5),[
                     rx('theta5'),[
                         tz(-1, name='pend5')]]]]],
     ty(-1), [
-        rx('theta6'),[
+        rx('theta6', kinematic=True),[
             tz(-0.5, mass=1, name='COM6'),[
                 tz(-0.5, name='pend6'),[
                     rx('theta7'),[
@@ -66,24 +45,44 @@ trep.potentials.Gravity(system, name="Gravity")
 # trep.forces.Damping(system, 0.1)
 
 # Input Torque
-trep.forces.ConfigForce(system, 'theta1', 'theta1-torque-middle')
-trep.forces.ConfigForce(system, 'theta4', 'theta4-torque-right')
-trep.forces.ConfigForce(system, 'theta6', 'theta6-torque-left')
-e = math.exp(1)
-def forcing1(t):
-    return (0.0*math.exp(-(t-5)**2), -7*math.exp(-(t-5)**2), 7*math.exp(-(t-5)**2))
+# trep.forces.ConfigForce(system, 'theta1', 'theta1-torque-middle')
+# trep.forces.ConfigForce(system, 'theta4', 'theta4-torque-right')
+# trep.forces.ConfigForce(system, 'theta6', 'theta6-torque-left')
 
-# Assign values to the system initial configuration
-pie = math.pi
-system.q = (pie/3,-2*pie/3,pie/3,0,-pie/2,0,pie/2)
+# def forcing(t):
+#     return (0.0*math.exp(-(t-5)**2), -7*math.exp(-(t-5)**2), 7*math.exp(-(t-5)**2))
 
 # Add constraints
 trep.constraints.PointToPoint2D(system,'yz','pend5','legConnection')
 trep.constraints.PointToPoint2D(system,'yz','pend7','legConnection')
 
+# Assign values to the system initial configuration
+pie = math.pi
+system.q = (pie/3,-2*pie/3,pie/3,0,-pie/2,0,pie/2)
+
+system.satisfy_constraints()
+
 # Simulate
 start = time.clock()
-(t, q) = simulate_system(system,forcing1)
+q0 = system.q
+qk2_0 = system.qk
+
+# Create and initialize the variational integrator
+mvi = trep.MidpointVI(system)
+mvi.initialize_from_configs(0.0, q0, dt, q0)
+
+# This is our simulation loop.  We save the results in two lists.
+q = [mvi.q2]
+t = [mvi.t2]
+while mvi.t1 < tf:
+    qk2 = list(qk2_0)
+    qk2[system.get_config('theta1').k_index] += -0.25*sin(0.5*mvi.t1)
+    qk2[system.get_config('theta4').k_index] += 0.25*sin(0.5*mvi.t1)
+    qk2[system.get_config('theta6').k_index] += -0.25*sin(0.5*mvi.t1)
+    mvi.step(mvi.t2+dt, (), tuple(qk2))
+    q.append(mvi.q2)
+    t.append(mvi.t2)
+
 finish = time.clock()
 
 # Display
