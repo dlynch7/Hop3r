8d4e50fc832209045d96c83a7b526149dbb23582
dlynch7 <daniellynch2021@u.northwestern.edu>
2018-02-26 12:53:01 -0600

    sweeping 3 hobby servos using a triangle wave

---
+++ Tiva/3servos/build/servocontrol.d
@@ -0,0 +1,32 @@
+build/servocontrol.o: src/servocontrol.c \
+ /usr/lib/gcc/arm-none-eabi/4.8.2/include/stdint.h \
+ /usr/include/newlib/stdint.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /usr/include/newlib/sys/features.h \
+ /usr/lib/gcc/arm-none-eabi/4.8.2/include/stdbool.h \
+ /usr/include/newlib/stdlib.h /usr/include/newlib/machine/ieeefp.h \
+ /usr/include/newlib/_ansi.h /usr/include/newlib/newlib.h \
+ /usr/include/newlib/sys/config.h \
+ /usr/lib/gcc/arm-none-eabi/4.8.2/include/stddef.h \
+ /usr/include/newlib/sys/reent.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/sys/_types.h /usr/include/newlib/machine/_types.h \
+ /usr/include/newlib/sys/lock.h /usr/include/newlib/machine/stdlib.h \
+ /home/daniel/Embedded/tivaware/inc/hw_ints.h \
+ /home/daniel/Embedded/tivaware/inc/hw_memmap.h \
+ /home/daniel/Embedded/tivaware/inc/hw_uart.h \
+ /home/daniel/Embedded/tivaware/inc/hw_gpio.h \
+ /home/daniel/Embedded/tivaware/inc/hw_timer.h \
+ /home/daniel/Embedded/tivaware/inc/hw_types.h \
+ /home/daniel/Embedded/tivaware/driverlib/pin_map.h \
+ /home/daniel/Embedded/tivaware/driverlib/rom.h \
+ /home/daniel/Embedded/tivaware/driverlib/rom_map.h \
+ /home/daniel/Embedded/tivaware/driverlib/debug.h \
+ /home/daniel/Embedded/tivaware/driverlib/systick.h \
+ /home/daniel/Embedded/tivaware/driverlib/interrupt.h \
+ /home/daniel/Embedded/tivaware/driverlib/sysctl.h \
+ /home/daniel/Embedded/tivaware/driverlib/uart.h \
+ /home/daniel/Embedded/tivaware/driverlib/udma.h \
+ /home/daniel/Embedded/tivaware/driverlib/gpio.h \
+ /home/daniel/Embedded/tivaware/driverlib/timer.h \
+ /usr/include/newlib/string.h /usr/include/newlib/sys/cdefs.h \
+ /usr/include/newlib/sys/string.h
