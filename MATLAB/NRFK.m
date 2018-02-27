aa6be528e95ba99c2b028b3d55413e358b4c2806
dlynch7 <daniellynch2021@u.northwestern.edu>
2018-02-26 22:08:38 -0600

    fixed convergence criterion

--- MATLAB/NRFK.m
+++ MATLAB/NRFK.m
@@ -12,10 +12,10 @@ function footPose = NRFK(qa,qu0,lengths,thresh,chain)
 %       qu = [theta2; theta3; phi2; phi3; psi2; psi3];
 %   lengths: the relevant dimensions of the robot, stored in a 10x1 column
 %       vector according to this order:
-%       lengths = [L1; L2; L3; L4; L5 L6; L7; L8; B1; B2], where
-%       B1, L1, L2, and L8 correspond to the theta-chain,
+%       lengths = [L1; L2; L3; L4; L5 L6; L7; L8; B1x; B2x; B1y; B2y], where
+%       B1x, B1y, L1, L2, and L8 correspond to the theta-chain,
 %       L3, L4, L7, and L8 correspond to the phi-chain, and
-%       B2, L5, L6, and L8 correspond to the psi-chain.
+%       B2x, B2y, L5, L6, and L8 correspond to the psi-chain.
 %   thresh: convergence threshold
 %   chain:  select which open sub-chain to use for FK
 %       1: use the theta-chain
@@ -35,12 +35,16 @@ function footPose = NRFK(qa,qu0,lengths,thresh,chain)
 
 convMeasure = thresh*10; % initialize convergence measure
 
+G0 = zeros(6,1);
+G1 = zeros(6,1);
+
 while(convMeasure > thresh)
-    G0 = constraintVector(qa,qu0,lengths);
+    [G0] = constraintVector(qa,qu0,lengths);
     Jc = constraintJacobian(qa,qu0,lengths);
     qu1 = qu0 - pinv(Jc)*G0;
-    G1 = constraintVector(qa,qu1,lengths);
-    convMeasure = abs(G0 - G1)/abs(G0); % compute convergence measure with candidate soln.
+    [G1] = constraintVector(qa,qu1,lengths);
+    convMeasure = norm(G0 - G1)/norm(G0); % compute convergence measure with candidate soln.
+    fprintf('convergence measure: %f\n',convMeasure);
     qu0 = qu1; % update approximate unactuated joint positions
 end % end Newton-Raphson root finding algorithm
 
@@ -48,13 +52,15 @@ theta = [qa(1); qu0(1); qu0(2)]; % theta-chain joint positions
 phi = [qa(2); qu0(3); qu0(4)]; % phi-chain joint positions
 psi = [qa(3); qu0(5); qu0(6)]; % psi-chain joint positions
 
+angles = transpose([theta(:), phi(:), psi(:)]);
+
 switch chain
     case 1 % calculate FK along theta-chain
-        [xF, yF, angF] = subchainFK(theta,1);
+        [xF, yF, angF] = subchainFK(angles,lengths,1);
     case 2 % calculate FK along phi-chain
-        [xF, yF, angF] = subchainFK(phi,2);
+        [xF, yF, angF] = subchainFK(angles,lengths,2);
     case 3 % calculate FK along psi-chain
-        [xF, yF, angF] = subchainFK(psi,3);
+        [xF, yF, angF] = subchainFK(angles,lengths,3);
     otherwise % handle errors
         disp('NRFK error: unknown open sub-chain selection');
 end
