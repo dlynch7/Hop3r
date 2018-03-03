a20e2a5e247adbbc9dc72ff9fab30dc0a75da0d9
dlynch7 <daniellynch2021@u.northwestern.edu>
2018-03-02 19:29:58 -0600

    version 0 of CAN bus, using TivaWare example code. Interrupt works but there is connection error, unknown.

---
+++ Tiva/CAN_prototype/RxNode/build/simple_rx.d
@@ -0,0 +1,18 @@
+build/simple_rx.o: src/simple_rx.c \
+ /usr/lib/gcc/arm-none-eabi/4.8.2/include/stdbool.h \
+ /usr/lib/gcc/arm-none-eabi/4.8.2/include/stdint.h \
+ /usr/include/newlib/stdint.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /usr/include/newlib/sys/features.h \
+ /home/daniel/Embedded/tivaware/inc/hw_can.h \
+ /home/daniel/Embedded/tivaware/inc/hw_ints.h \
+ /home/daniel/Embedded/tivaware/inc/hw_memmap.h \
+ /home/daniel/Embedded/tivaware/inc/hw_types.h \
+ /home/daniel/Embedded/tivaware/driverlib/can.h \
+ /home/daniel/Embedded/tivaware/driverlib/gpio.h \
+ /home/daniel/Embedded/tivaware/driverlib/interrupt.h \
+ /home/daniel/Embedded/tivaware/driverlib/pin_map.h \
+ /home/daniel/Embedded/tivaware/driverlib/sysctl.h \
+ /home/daniel/Embedded/tivaware/driverlib/uart.h \
+ /home/daniel/Embedded/tivaware/utils/uartstdio.h \
+ /usr/lib/gcc/arm-none-eabi/4.8.2/include/stdarg.h
---
+++ Tiva/CAN_prototype/TxNode/build/startup_gcc.d
@@ -0,0 +1,8 @@
+build/startup_gcc.o: src/startup_gcc.c \
+ /usr/lib/gcc/arm-none-eabi/4.8.2/include/stdbool.h \
+ /usr/lib/gcc/arm-none-eabi/4.8.2/include/stdint.h \
+ /usr/include/newlib/stdint.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /usr/include/newlib/sys/features.h \
+ /home/daniel/Embedded/tivaware/inc/hw_nvic.h \
+ /home/daniel/Embedded/tivaware/inc/hw_types.h
