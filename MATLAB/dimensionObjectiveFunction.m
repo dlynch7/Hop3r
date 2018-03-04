5270c454bf1e00c62355180bf1213f41a8d59f09
dlynch7 <daniellynch2021@u.northwestern.edu>
2018-03-04 13:16:37 -0600

    searching foot workspace for reachable subspace

--- MATLAB/dimensionObjectiveFunction.m
+++ MATLAB/dimensionObjectiveFunction.m
@@ -1,4 +1,4 @@
-function J = dimensionObjectiveFunction(lengths, angles, wrench, twist, torqueWeight, velWeight)
+function J = dimensionObjectiveFunction(lengths, wrench, torqueWeight, volWeight)
 %% Description:
 % Computes a quadratic objective function (for optimization) using torques
 % and joint velocities, calculated from an input wrench and an input twist
@@ -9,39 +9,56 @@ function J = dimensionObjectiveFunction(lengths, angles, wrench, twist, torqueWe
 % lengths: 12x1 array
 %   [L1; L2; L3; L4; L5; L6; L7; L8; B1x; B1y; B2x; B2y];
 
-% angles: 3x3 array
-%   [theta1 theta2  theta3;
-%    phi1   phi2    phi3;
-%    psi1   psi2    psi3];
-
 % wrench: 3x1 array
 %   [Fx; Fy; Mz];
 
-% twist: 3x1 array
-%   [vx; vy; wz];
+% torqueWeight: scalar weight
 
-% torqueWeight: 3x3 symmetric positive-definite array.
-% Simplest case is a multiple of eye(3);
-%   [tW1a tW1b tW1c;
-%    tW2a tW2b tW2c;
-%    tW3a tW3b tW3c];
-
-% velWeight: 3x3 symmetric positive-definite array.
-% Simplest case is a multiple of eye(3);
-%   [vW1a vW1b vW1c;
-%    vW2a vW2b vW2c;
-%    vW3a vW3b vW3c];
+% volWeight: scalar weight
 
 % Output:
 % J: scalar value of the objective function
 
+%% Define the end-effector workspace that will be searched for the "achievable" workspace:
+% footXarray = linspace(-0.25, 0.25, 11);
+footXarray = linspace(-0.05, 0.05, 11);
+% footYarray = linspace(-0.5, 0.0, 11);
+footYarray = linspace(-0.25, -0.25, 11);
+% footAnglearray = linspace(-pi/4, -3*pi/4, 11);
+footAnglearray = linspace(-pi/2, -pi/2, 11);
+
+%% Brute-force exploration of workspace:
+for i = 1:length(footXarray)
+    for j = 1:length(footYarray)
+        for k = 1:length(footAnglearray)
+            try
+                footPose = [footXarray(i); footYarray(j); footAnglearray(k)];
+                [tempAngles] = subchainIK(footPose,lengths,0);
+                if 1
+                %                 if checkJointLimits(tempAngles)
+                    anglesD{i,j,k} = tempAngles.*(180/pi);
+                    plotRobot(footPose,lengths,tempAngles);
+                    fprintf(['passed: x = ',num2str(footXarray(i)),', y = ',...
+                        num2str(footYarray(j)),', ang = ',num2str(footAnglearray(k)),'\n']);
+                else
+                    anglesD{i,j,k} = NaN;
+                    fprintf(['failed: x = ',num2str(footXarray(i)),', y = ',...
+                        num2str(footYarray(j)),', ang = ',...
+                        num2str(footAngleDarray(k)),' exceeds joint limits.\n']);
+                end
+            catch
+                anglesD{i,j,k} = NaN;
+                fprintf(['failed: x = ',num2str(footXarray(i)),', y = ',...
+                        num2str(footYarray(j)),', ang = ',...
+                        num2str(footAnglearray(k)),' is unsolvable.\n']);
+            end % end try/catch
+        end
+    end
+end
+
 %% Calculate joint torques (Nm) from input wrench:
 torques = wrench2torques(angles, lengths, wrench);
 
-%% Calculate joint velocities (rad/s) from input twist:
-jointVels = twist2vels(angles, lengths, twist);
-
 %% Calculate value of objective function:
-J = (transpose(torques)*torqueWeight*torques) + ...
-    (transpose(jointVels)*velWeight*jointVels);
+J = torqueWeight*(torqueMax^2) + volWeight*(volWorkspace^2);
 end
