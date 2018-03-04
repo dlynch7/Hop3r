a178a939af395bcb868fae52887eb8e234e47ba1
dlynch7 <daniellynch2021@u.northwestern.edu>
2018-03-04 13:21:19 -0600

    modified to admit 12 dimensions instead of 8

--- MATLAB/NRqu.m
+++ MATLAB/NRqu.m
@@ -12,10 +12,10 @@ function qu = NRqu(qa,qu0,lengths,thresh)
 %       qu = [theta2; theta3; phi2; phi3; psi2; psi3];
 %   lengths: the relevant dimensions of the robot, stored in a 10x1 column
 %       vector according to this order:
-%       lengths = [L1; L2; L3; L4; L5 L6; L7; L8; B1; B2], where
-%       B1, L1, L2, and L8 correspond to the theta-chain,
+%       lengths = [L1; L2; L3; L4; L5 L6; L7; L8; B1x; B2x; B1y; B2y], where
+%       B1x, B1y, L1, L2, and L8 correspond to the theta-chain,
 %       L3, L4, L7, and L8 correspond to the phi-chain, and
-%       B2, L5, L6, and L8 correspond to the psi-chain.
+%       B2x, B2y, L5, L6, and L8 correspond to the psi-chain.
 %   thresh: convergence threshold
 %
 % Outputs:
--- MATLAB/exploreWorkspace.m
+++ MATLAB/exploreWorkspace.m
@@ -15,9 +15,11 @@ L5 = L1; % theta-chain and psi-chain are symmetric
 L6 = L2; % theta-chain and psi-chain are symmetric
 L7 = 0.5;
 L8 = 1.5;
-B1 = 1;
-B2 = B1; % theta-chain and psi-chain are symmetric
-lengths = [L1 L2 L3 L4 L5 L6 L7 L8 B1 B2]; % link lengths
+B1x = 1;
+B2x = B1x; % theta-chain and psi-chain are symmetric
+B1y = 0;
+B2y = B1y; % theta-chain and psi-chain are symmetric
+lengths = [L1 L2 L3 L4 L5 L6 L7 L8 B1x B2x B1y B2y]; % link lengths
 
 %% Explore workspace:
 footXarray = -1:0.5:1;
@@ -31,7 +33,8 @@ for i = 1:length(footXarray)
             try
                 footPose = [footXarray(i); footYarray(j); footAngleDarray(k)*(pi/180)];
                 [tempAngles] = subchainIK(footPose,lengths,0);
-                if checkJointLimits(tempAngles)
+%                 if checkJointLimits(tempAngles)
+                if 1
                     anglesD{i,j,k} = tempAngles.*(180/pi);
                     plotRobot(footPose,lengths,tempAngles);
                     fprintf(['passed: x = ',num2str(footXarray(i)),', y = ',...
