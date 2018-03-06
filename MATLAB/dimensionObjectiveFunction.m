50fa3bf67605c43e6f124c24886fcaa363161dc3
dlynch7 <daniellynch2021@u.northwestern.edu>
2018-03-06 14:41:01 -0600

    generating point cloud of reachable foot poses

--- MATLAB/dimensionObjectiveFunction.m
+++ MATLAB/dimensionObjectiveFunction.m
@@ -1,4 +1,4 @@
-function J = dimensionObjectiveFunction(symlengths, wrench, torqueWeight, volWeight)
+function J = dimensionObjectiveFunction(symlengths, wrench, torqueWeight, volWeight, resolution, vizOption)
 %% Description:
 % Computes a quadratic objective function (for optimization) using torques
 % and joint velocities, calculated from an input wrench and an input twist
@@ -21,6 +21,13 @@ function J = dimensionObjectiveFunction(symlengths, wrench, torqueWeight, volWei
 
 % volWeight: scalar weight
 
+% resolution: how many points along each dimension of the workspace. 11 is
+% fast. 21 is slow.
+
+% vizOption: 0 or 1
+%   0: do not output visualization of reachable workspace
+%   1: output a visualization of the reachable workspace
+
 % Output:
 % J: scalar value of the objective function
 
@@ -30,9 +37,9 @@ lengths = [symlengths(1); symlengths(2); symlengths(3); symlengths(4);
            symlengths(7); symlengths(7); symlengths(8); symlengths(8)];
 
 %% Define the end-effector workspace that will be searched for the "achievable" workspace:
-footXarray = linspace(-0.25, 0.25, 11);
-footYarray = linspace(-0.25, -0.0, 11);
-footAnglearray = linspace(-3*pi/4, -pi/4, 11);
+footXarray = linspace(-0.25, 0.25, resolution);
+footYarray = linspace(-0.25, -0.0, resolution);
+footAnglearray = linspace(-3*pi/4, -pi/4, resolution);
 % footXarray = linspace(-0.0, 0.0, 11);
 % footYarray = linspace(-0.25, -0.25, 11);
 % footAnglearray = linspace(-80*pi/180, -80*pi/180, 11);
@@ -55,12 +62,10 @@ for i = 1:length(footXarray)
                     torques = wrench2torques(tempAngles, lengths, space_wrench);
                     torqueMag = norm(torques);
                     torqueMagArray = horzcat(torqueMagArray,torqueMag);
-%                     fprintf(['passed: x = ',num2str(footXarray(i)),', y = ',...
-%                         num2str(footYarray(j)),', ang = ',num2str(footAnglearray(k)),'\n']);
-                else
-%                     fprintf(['failed: x = ',num2str(footXarray(i)),', y = ',...
-%                         num2str(footYarray(j)),', ang = ',...
-%                         num2str(footAngleDarray(k)),' exceeds joint limits.\n']);
+%                     if vizOption==1
+%                         plot3(footPose(1),footPose(2),footPose(3),'b.');
+%                         hold on
+%                     end
                 end
             catch
 %                 fprintf(['failed: x = ',num2str(footXarray(i)),', y = ',...
@@ -69,6 +74,39 @@ for i = 1:length(footXarray)
             end % end try/catch
         end
     end
+end
+
+if vizOption==1
+    reachablePoseT = reachablePose';
+
+    % the following method is from
+    % https://stackoverflow.com/questions/5492806/plotting-a-surface-from-a-set-of-interior-3d-scatter-points-in-matlab
+    DT = delaunayTriangulation(reachablePoseT);  % Create the tetrahedral mesh
+    hullFacets = convexHull(DT);       % Find the facets of the convex hull
+
+    figure;
+    % Plot the scattered points:
+    subplot(2,2,1);
+    scatter3(reachablePoseT(:,1),reachablePoseT(:,2),reachablePoseT(:,3),'.');
+    axis square;
+    title('Interior points');
+
+    %# Plot the tetrahedral mesh:
+    subplot(2,2,2);
+    tetramesh(DT);
+    axis square;
+    title('Tetrahedral mesh');
+
+    %# Plot the 3-D convex hull:
+    subplot(2,2,3);
+    trisurf(hullFacets,DT.Points(:,1),DT.Points(:,2),DT.Points(:,3),'FaceColor','c')
+    axis square;
+    title('Convex hull');
+    
+%     xlabel('Foot X')
+%     ylabel('Foot Y')
+%     zlabel('Foot angle')
+%     title('Reachable foot workspace')
 end
 
 %% Calculate "volume" of reachable workspace:
