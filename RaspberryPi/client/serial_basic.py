b513e956e0f56bb7c6082d649d2935c575370c5e
dlynch7 <daniellynch2021@u.northwestern.edu>
2018-03-22 14:28:37 -0500

    added timeouts and timeout notifications

--- RaspberryPi/client/serial_basic.py
+++ RaspberryPi/client/serial_basic.py
@@ -1,16 +1,39 @@
 import serial
-# import numpy as np
+import time
 
-ser = serial.Serial('/dev/ttyUSB0',115200) # open serial port
+timeout = 5;
+
+ser = serial.Serial(port='/dev/ttyUSB0',baudrate=115200,timeout=timeout) # open serial port
 print(ser.name)                     # check which port was really opened
 
-NSAMPLES = int(ser.readline());
+now = time.time()
+NSAMPLES = ser.readline();
+deltat = time.time() - now;
 
-get = [0]*NSAMPLES
+if deltat < timeout:
+    NSAMPLES = int(NSAMPLES);
+    print "Expecting %d samples." % NSAMPLES
+    get = [0]*NSAMPLES # create array to store received data
+    received = 0;
 
-for i in range(NSAMPLES):
-    line = ser.readline()
-    get[i] = line
-    print get[i]
+    for i in range(NSAMPLES):
+        now = time.time()
 
-ser.close();
+        line = ser.readline()
+        get[i] = line
+        print get[i]
+
+        deltat = time.time() - now;
+
+        if deltat < timeout:
+            received += 1
+
+    print received
+    if received < NSAMPLES:
+        print "Serial read timeout occurred. Expected %d samples, received %d samples." % (NSAMPLES, received)
+
+else:
+    print "Serial read timeout occurred before the number of samples was sent. Check connection."
+
+
+ser.close()
