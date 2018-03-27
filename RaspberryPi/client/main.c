11183ae239cc26b1e0dd70ceec46206471b90404
dlynch7 <daniellynch2021@u.northwestern.edu>
2018-03-27 17:54:21 -0500

    adding CAN functionality

--- RaspberryPi/client/main.c
+++ RaspberryPi/client/main.c
@@ -1,18 +1,83 @@
+/*
+* CAN interface adapted from cantest.c, from www.skpang.co.uk
+* Multithreading by yours truly.
+* Dan Lynch
+* 3-27-2018
+*/
+
+/*
+ *  $Id$
+ */
+
+/*
+ * cansend.c - simple command line tool to send CAN-frames via CAN_RAW sockets
+ *
+ * Copyright (c) 2002-2007 Volkswagen Group Electronic Research
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of Volkswagen nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * Alternatively, provided that this notice is retained in full, this
+ * software may be distributed under the terms of the GNU General
+ * Public License ("GPL") version 2, in which case the provisions of the
+ * GPL apply INSTEAD OF those given above.
+ *
+ * The provided data structures and external interfaces from this code
+ * are not restricted to be used by modules with a GPL compatible license.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ *
+ * Send feedback to <linux-can@vger.kernel.org>
+ *
+ */
+
+/* 20-05-15 Modified to sent a message out in code rather then from parameter.
+
+	www.skpang.co.uk
+*/
+
 #include <errno.h>
+#include <linux/can.h>
+#include <linux/can/raw.h>
+#include <net/if.h>
 #include <pthread.h>
-// #include <sched.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/ioctl.h>
 #include <sys/types.h>	// needed for getpid()
+#include <time.h>
 #include <unistd.h>			// needed for getpid()
 #include <wiringPi.h>
 #include <wiringSerial.h>
+
+#include "linux-can-utils/lib.h"
 #include "circ_buffer.h"
 
-void *write_thread();
-void *read_thread();
+void *CAN_thread();
+void *UART_thread();
 pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
 uint8_t begin; // read and write threads must wait for begin = 1
 int serial_port;
@@ -22,9 +87,6 @@ char writemsg[10] = {};
 
 uint8_t main(void) {
   int rc1, rc2;
-  // pthread_attr_t tattr;
-  // int ret;
-  // int newprio = 20;
   uint8_t writePermission = 0;
 
   begin = 0; // reading and writing cannot commence
@@ -57,8 +119,8 @@ uint8_t main(void) {
 
   /****************************************************************************
 	*	Create two independent threads.
-  * One thread will write to a buffer.
-  * The other thread will read from the buffer.
+  * CAN_thread will write to a buffer.
+  * UART_thread will read from the buffer.
 	****************************************************************************/
 
   // // initialized with default attributes:
@@ -72,10 +134,10 @@ uint8_t main(void) {
 
 
 	pthread_t thread1, thread2;
-  if ( (rc1=pthread_create(&thread1,NULL,&write_thread,NULL)) ) {
+  if ( (rc1=pthread_create(&thread1,NULL,&CAN_thread,NULL)) ) {
 		printf("Thread creation failed: %d\n", rc1);
 	}
-	if ( (rc2=pthread_create(&thread2,NULL,&read_thread,NULL)) ) {
+	if ( (rc2=pthread_create(&thread2,NULL,&UART_thread,NULL)) ) {
 		printf("Thread creation failed: %d\n", rc2);
 	}
 
@@ -100,9 +162,60 @@ uint8_t main(void) {
   return 0;
 }
 
-void *write_thread() {
+void *CAN_thread() {
   uint16_t i;
   unsigned int nextWriteTime;
+
+  /****************************************************************************
+  * Set up CAN raw socket
+  ****************************************************************************/
+  // if you don't have access to the CAN bus, comment out from the next line:
+  pthread_mutex_lock(&mutex1);
+  int s; // can raw socket
+  int nbytes;
+  struct sockaddr_can addr;
+  struct can_frame frame;
+  struct ifreq ifr;
+
+  /* open socket */
+	if ((s = socket(PF_CAN, SOCK_RAW, CAN_RAW)) < 0) {
+		perror("socket");
+    // TO-DO: ERROR HANDLING
+    // return 1;
+	}
+
+  addr.can_family = AF_CAN;
+
+	strcpy(ifr.ifr_name, "can0");
+	if (ioctl(s, SIOCGIFINDEX, &ifr) < 0) {
+		perror("SIOCGIFINDEX");
+    // TO-DO: ERROR HANDLING
+    // return 1;
+	}
+	addr.can_ifindex = ifr.ifr_ifindex;
+
+  /* disable default receive filter on this RAW socket */
+	/* This is obsolete as we do not read from the socket at all, but for */
+	/* this reason we can remove the receive list in the Kernel to save a */
+	/* little (really a very little!) CPU usage.                          */
+	setsockopt(s, SOL_CAN_RAW, CAN_RAW_FILTER, NULL, 0);
+
+	if (bind(s, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+		perror("bind");
+    // TO-DO: ERROR HANDLING
+		// return 1;
+	}
+
+  printf("CAN socket set up successfully!\n");
+  pthread_mutex_unlock(&mutex1);
+  // if you don't have access to the CAN bus, comment out up to the line above.
+  // Make sure both mutex lock and unlock are either both commented out or
+  // neither commented out.
+
+  /****************************************************************************
+  * Wait for permission to begin,
+  * then send/receive via CAN and put relevant data into circular buffer.
+  ****************************************************************************/
 
   while(!begin) {;}
   nextWriteTime = millis() + periodms_write;
@@ -120,10 +233,15 @@ void *write_thread() {
   printf("Write thread has completed.\n");
 }
 
-void *read_thread() {
+void *UART_thread() {
   uint16_t j;
   uint16_t bufferval;
   unsigned int nextReadTime;
+
+  /****************************************************************************
+  * Wait for permission to begin,
+  * then get relevant data from circular buffer and send via UART
+  ****************************************************************************/
 
   while(!begin) {;}
   nextReadTime = millis() + periodms_read;
