4d463cfc1c242d466b942df664098df0b4b55087
dlynch7 <daniellynch2021@u.northwestern.edu>
2018-04-04 14:10:02 -0500

    first commit: Raspberry Pi library for the robot's kinematics

---
+++ RaspberryPi/client/kinematic.c
@@ -0,0 +1,26 @@
+// kinematic.c provides functions that were developed in MATLAB
+// and can be found at
+// https://github.com/dlynch7/Hop3r/tree/master/MATLAB/Kinematic
+
+#include <stdio.h>
+#include <stdint.h>
+#include <math.h>
+#include "kinematic.h"
+
+// Define link lengths
+#define L1  0.0530
+#define L2  0.1390
+#define L3  0.0970
+#define L4  0.0983
+#define L5  0.0530
+#define L6  0.1390
+#define L7  0.0692
+#define L8  0.1018
+#define B1X 0.0573
+#define B2X 0.0573
+#define B1Y 0.0082
+#define B2Y 0.0082
+
+int8_t geomFK(float *qa, float *qu, float *lengths, float *footPose, uint8_t solOption) {
+    // return value indicates error
+}
