8713d796e2f809743653c9cb01f6317970084ad7
dlynch7 <daniellynch2021@u.northwestern.edu>
2018-04-13 15:19:55 -0500

    added #defines for drive states

--- Tiva/CAN_Copley/ver0/src/copley_accelus.c
+++ Tiva/CAN_Copley/ver0/src/copley_accelus.c
@@ -20,6 +20,7 @@
 //*****************************************************************************
 //
 // Copley Accelus op-codes
+// (see "Binary-Serial-Interface-Operation-Application-Notes.pdf")
 //
 //*****************************************************************************
 #define OPCODE_NO_OP 0
@@ -41,9 +42,41 @@
 
 //*****************************************************************************
 //
+// Copley Accelus state options (see parameter_dictionary.pdf)
+//
+//*****************************************************************************
+#define STATE_DISABLED 0
+#define STATE_CURR_PRG 1
+#define STATE_CURR_ANR 2
+#define STATE_CURR_PWM 3
+#define STATE_CURR_FNC 4
+#define STATE_CURR_UVM 5
+// 6-10 reserved for future use
+#define STATE_VEL_PRG 11
+#define STATE_VEL_ANR 12
+#define STATE_VEL_PWM 13
+#define STATE_VEL_FNC 14
+// 15-20 reserved for future use
+#define STATE_POS_PRG 21
+#define STATE_POS_ANR 22
+#define STATE_POS_DIG 23
+#define STATE_POS_FNC 24
+#define STATE_POS_CAM 25
+// 26-29 reserved
+#define STATE_CANOPEN_ETHERCAT 30
+#define STATE_MICROSTEP_TRJ 31
+// 32 reserved for future use
+#define STATE_MICROSTEP_DIG 33
+#define STATE_MICROSTEP_FNC 34
+#define STATE_MICROSTEP_CAM 35
+// 36-39 reserved for future use
+
+//*****************************************************************************
+//
 // Other #defines
 //
 //*****************************************************************************
+#define HEADER_LEN 4
 #define COPLEY_NODE_NUMBER 0
 
 //*****************************************************************************
@@ -51,10 +84,13 @@
 // Private functions (used only in copley_accelus.c):
 //
 //*****************************************************************************
-static uint8_t gen_checksum(uint8_t nbytes, uint8_t *tx_packet) { // generate checksum based on packet
+static uint8_t gen_checksum(uint8_t *tx_packet) { // generate checksum based on packet
   // from https://youtu.be/hGEtd86k3dU
   uint8_t i = 0;
+  uint8_t nbytes = 0;
   uint8_t checksum = 0;
+
+  nbytes = sizeof(tx_packet)/sizeof(tx_packet[0]);
   // XOR each byte in the packet with the previous byte:
   for (i = 0; i < nbytes; i++) {
     checksum ^= tx_packet[i];
@@ -65,18 +101,18 @@ static uint8_t gen_checksum(uint8_t nbytes, uint8_t *tx_packet) { // generate ch
   return checksum;
 }
 
-static uint8_t copley_send() { // send command to Copley Accelus
-  uint8_t checksum = 0;
-  // assemble packet without checksum
-  // generate checksum from packet
-  // insert checksum into packet
-  // send entire packet
-}
-static uint32_t copley_recv() { // receive response from Copley Accelus
-  // receive packet
-  // parse packet
-  // check checksum and error code
-}
+// static uint8_t copley_send() { // send command to Copley Accelus
+//   uint8_t checksum = 0;
+//   // assemble packet without checksum
+//   // generate checksum from packet
+//   // insert checksum into packet
+//   // send entire packet
+// }
+// static uint32_t copley_recv() { // receive response from Copley Accelus
+//   // receive packet
+//   // parse packet
+//   // check checksum and error code
+// }
 
 //*****************************************************************************
 //
@@ -84,16 +120,42 @@ static uint32_t copley_recv() { // receive response from Copley Accelus
 //
 //*****************************************************************************
 uint8_t set_copley_mode(uint8_t copley_mode) {
+  uint8_t checksum = 0;
+  uint8_t tx_packet[HEADER_LEN + 2] = {};
 
+  // header:
+  tx_packet[0] = COPLEY_NODE_NUMBER;
+  tx_packet[1] = checksum; // checksum is still 0 at this point, it will change later
+  tx_packet[2] = 2; // 2 16-bit words will follow header
+  tx_packet[3] = OPCODE_SET_VAR_VAL;
+
+  // body:
+  tx_packet[4] = 0;
+  tx_packet[5] = 0x24; // register to write to: "desired state" (see Parameter Dictionary)
+  tx_packet[6] = 0;
+  tx_packet[7] = copley_mode;
+
+  // calculate checksum:
+  checksum = gen_checksum(tx_packet)
+  tx_packet[1] = checksum;
 }
 
 uint8_t get_copley_mode(void) {
-
+  uint8_t checksum = 0;
+  uint8_t tx_packet[HEADER_LEN + 2] = {};
+  tx_packet[0] = COPLEY_NODE_NUMBER;
+  tx_packet[1] = checksum; // checksum is still 0 at this point, it will change later
+  tx_packet[2] = 1; // 1 16-bit word will follow header
+  tx_packet[3] = OPCODE_GET_VAR_VAL;
+  tx_packet[4] = 0;
+  tx_packet[5] = 0x24; // register to read from: "desired state" (see Parameter Dictionary)
+  checksum = gen_checksum(tx_packet)
+  tx_packet[1] = checksum;
 }
 
 uint8_t set_current_mA(int16_t cur_ref_mA) {
-
+  return 0;
 }
 uint16_t get_current_mA(void) {
-
+  return 0;
 }
